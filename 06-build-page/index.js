9a04176fb97dc71752fadee24466a582f8f4a230
Sviatlana <sveta_04071987@mail.ru>
2022-05-23 23:04:15 +0300

    init: add html builder files

---
+++ 06-build-page/index.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const fs = require('fs');
+const fsPromises = fs.promises;
+const FILENAME = 'style.css';
+const styles = [];
+
+async function createHTML(template, components, assets, dst) {
+  const templatePath = path.join(__dirname, template);
+  const componentsPath = path.join(__dirname, components);
+  const dstPath = path.join(__dirname, dst);
+  const assetsPath = path.join(__dirname, assets);
+  createTemplate(templatePath, componentsPath, dstPath);
+  createCSS('styles', dst);
+  copyDir(assetsPath, path.join(dstPath, assets));
+}
+
+async function createTemplate(templatePath, componentsPath, dstPath) {
+  const html = await fsPromises.readFile(templatePath);
+  const componentFiles = await fsPromises.readdir(componentsPath);
+  let content = html.toString();
+  for(let componentFile of componentFiles) {
+    const stats = await fsPromises.stat(path.join(componentsPath, componentFile));
+    if (stats.isFile() && path.extname(componentFile) === '.html') {
+      let componentName = path.basename(componentFile, path.extname(componentFile));
+      let componentContent = await fsPromises.readFile(path.join(componentsPath, componentFile));
+
+      content = content.replace(new RegExp(`{{${componentName}}}`), componentContent.toString());
+    }
+  }
+  await fsPromises.mkdir(dstPath, { recursive: true });
+  await fsPromises.writeFile(path.join(dstPath, 'index.html'), content);
+}
+
+async function createCSS(src, dst) {
+  const srcPath = path.join(__dirname, src);
+  const dstFilePath = path.join(__dirname, dst, FILENAME);
+  let files = await fsPromises.readdir(srcPath);
+  for (let file of files) {
+    const stats = await fsPromises.stat(path.join(srcPath, file));
+    if (stats.isFile() && path.extname(file) === '.css') {
+      let stylesBuffer = await fsPromises.readFile(path.join(srcPath, file));
+      styles.push(stylesBuffer);
+    }
+  }
+  let start = true;
+  for (let style of styles) {
+    if (start) {
+      await fsPromises.writeFile(dstFilePath, style);
+      start = false;
+    } else {
+      await fsPromises.writeFile(dstFilePath, style, { flag: 'a' });
+    }
+  }
+}
+
+async function copyDir(srcPath, dstPath) {
+  await fsPromises.mkdir(dstPath, { recursive: true });
+  let files = await fsPromises.readdir(dstPath);
+  for (let file of files) {
+    await fsPromises.rm(path.join(dstPath, file));
+  }
+  files = await fsPromises.readdir(srcPath);
+  for (let file of files) {
+    let stats = await fsPromises.stat(path.join(srcPath, file));
+    if (!stats.isDirectory()) {
+      await fsPromises.copyFile(path.join(srcPath, file), path.join(dstPath, file));
+    } else {
+      await copyDir(path.join(srcPath, file), path.join(dstPath, file));
+    }
+  }
+}
+
+createHTML('template.html', 'components', 'assets', 'project-dist');
